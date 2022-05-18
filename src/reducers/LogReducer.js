37f12ef4624903ef1980b0eea8b97cc4680791a8
George <george9040@abv.bg>
2022-05-18 17:10:29 +0300

    Added redux

---
+++ src/reducers/LogReducer.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (state, acition ) => {
+    switch (action.type) {
+        case value:
+            return 
+        default:
+            return state
+    }
+}
